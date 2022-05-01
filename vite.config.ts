48486171359cf38813851524fe31dc84dbb1348c
Mungunbagana Batzorig <MB.Bagana@gmail.com>
2022-05-01 21:14:38 +0800

    changed vite config url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "https://v3.mend.mn/",
+  base: "http://khan.bits.mn/",
   // server: {
   //   host: true,
   // },
